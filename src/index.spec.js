f1fa5347f45c27b51216fa0994cec9bbddc35748
Andrew Shim <andrewkshim92@gmail.com>
2017-09-21 22:41:21 -0700

    Added tests for refs now that withRig can take string refs

--- src/index.spec.js
+++ src/index.spec.js
@@ -1,6 +1,7 @@
 import './test.setup'
 
 import React from 'react'
+import ReactDOM from 'react-dom'
 import sinon from 'sinon'
 import test from 'tape'
 import { shallow, mount } from 'enzyme'
@@ -12,7 +13,7 @@ import {
 } from './index'
 
 test('withAnimatronics creates a valid React element', assert => {
-  const Animated = withAnimatronics(() => <div/>);
+  const Animated = withAnimatronics()(() => <div/>);
   const element = React.createElement(Animated);
   assert.true(React.isValidElement(element));
   assert.end();
@@ -20,7 +21,7 @@ test('withAnimatronics creates a valid React element', assert => {
 
 test('withAnimatronics supplies runAnimation() to the wrapped component', assert => {
   const Base = () => <div/>;
-  const Animated = withAnimatronics(Base);
+  const Animated = withAnimatronics()(Base);
   const wrapper = shallow(<Animated/>);
   const runAnimation = wrapper.find(Base).prop('runAnimation');
   assert.true(typeof runAnimation === 'function');
@@ -28,15 +29,19 @@ test('withAnimatronics supplies runAnimation() to the wrapped component', assert
 });
 
 test('withRig creates a valid React element', assert => {
-  const Rigged = withRig(() => <div/>);
+  const Rigged = withRig('base', { useStringRefs: true })(() => <div/>);
   const element = React.createElement(Rigged);
   assert.true(React.isValidElement(element));
   assert.end();
 });
 
 test('withRig registers the component when mounted', assert => {
-  const Base = () => <div/>;
-  const Rigged = withRig(Base);
+  class Base extends React.Component {
+    render() {
+      return <div/>;
+    }
+  }
+  const Rigged = withRig('base', { useStringRefs: true })(Base);
   const registerComponent = sinon.spy()
   const wrapper = mount(<Rigged/>, {
     context: {
@@ -51,8 +56,12 @@ test('withRig registers the component when mounted', assert => {
 });
 
 test('withRig unregisters the component when unmounted', assert => {
-  const Base = () => <div/>;
-  const Rigged = withRig(Base);
+  class Base extends React.Component {
+    render() {
+      return <div/>;
+    }
+  }
+  const Rigged = withRig('base', { useStringRefs: true })(Base);
   const unregisterComponent = sinon.spy()
   const wrapper = mount(<Rigged/>, {
     context: {
@@ -64,6 +73,32 @@ test('withRig unregisters the component when unmounted', assert => {
   });
   wrapper.unmount();
   assert.true(unregisterComponent.calledOnce);
+  assert.end();
+});
+
+test('withRig sets a refs', assert => {
+  class Base extends React.Component {
+    render() {
+      return <div/>;
+    }
+  }
+
+  const Rigged = withRig('base', { useStringRefs: true })(Base);
+  let actualDomRef;
+
+  const wrapper = mount(<Rigged/>, {
+    context: {
+      animatronics: {
+        registerComponent: ({ domRef }) => {
+          actualDomRef = domRef;
+        },
+        unregisterComponent: () => {},
+      },
+    },
+  });
+
+  const expectedDomRef = wrapper.find('div').node;
+  assert.equal(actualDomRef, expectedDomRef);
   assert.end();
 });
 
