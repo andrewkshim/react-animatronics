9c75420f96780b2f9bbcd6a568717189cf8ab5f2
Andrew Shim <andrewkshim92@gmail.com>
2017-09-22 23:01:38 -0700

    ✨: Allow clients to configure animations with springs

--- src/index.js
+++ src/index.js
@@ -17,6 +17,7 @@ const IS_RAF_AVAILABLE = (
   && window.requestAnimationFrame
 );
 const MS_PER_ANIMATION_FRAME = 1000 / 60; // 60 fps
+const SECONDS_PER_ANIMATION_FRAME = MS_PER_ANIMATION_FRAME / 1000;
 const DEFAULT_EASING_FN = BezierEasing(0.4, 0.0, 0.2, 1);
 
 const DEFAULT_REQUEST_ANIMATION_FRAME = IS_RAF_AVAILABLE
@@ -28,7 +29,7 @@ const DEFAULT_CANCEL_ANIMATION_FRAME = IS_RAF_AVAILABLE
   : clearTimeout
 
 const BETWEEN_PAREN_REGEX = /\(([^)]+)\)/;
-const NUMBER_REGEX = /\d+/;
+const NUMBER_REGEX = /\d+(\.\d+)?/;
 
 
 //==========================================================
@@ -56,10 +57,6 @@ const parseStyle = style => {
       unit: style.slice(value.length),
     };
   }
-}
-
-const calculateDifference = ({ parsedStartStyle, parsedEndStyle }) => {
-  return parsedEndStyle.value - parsedStartStyle.value;
 }
 
 const updateStyleValue = (parsedStyle, delta) => {
@@ -107,7 +104,7 @@ const handleAnimatronicsAction = (state, { type, payload }) => {
   ;
 }
 
-const calculateUpdatedStyle = ({
+const calculateTimedUpdatedStyle = ({
   startValue,
   endValue,
   easingFn,
@@ -117,7 +114,7 @@ const calculateUpdatedStyle = ({
   const normalizedDuration = duration === 0 ? elapsedTime : duration;
   const parsedStartStyle = parseStyle(startValue);
   const parsedEndStyle = parseStyle(endValue);
-  const difference = calculateDifference({ parsedStartStyle, parsedEndStyle });
+  const difference = parsedEndStyle.value - parsedStartStyle.value;
   const parsedUpdatedStyle = updateStyleValue(
     parsedStartStyle,
     difference * easingFn( elapsedTime / normalizedDuration ),
@@ -125,7 +122,7 @@ const calculateUpdatedStyle = ({
   return reconstructStyle(parsedUpdatedStyle);
 }
 
-const updateRigStyles = ({
+const updateTimedRigStyles = ({
   rigRef,
   startStyles,
   endStyles,
@@ -134,7 +131,7 @@ const updateRigStyles = ({
   elapsedTime,
 }) => {
   Object.keys(startStyles).forEach(styleName => {
-    const updatedStyle = calculateUpdatedStyle({
+    const updatedStyle = calculateTimedUpdatedStyle({
       startValue: startStyles[styleName],
       endValue: endStyles[styleName],
       easingFn,
@@ -145,6 +142,54 @@ const updateRigStyles = ({
   });
 }
 
+const updateSpringRigStyles = ({
+  rigRef,
+  currentStyles,
+  currentVelocities,
+  damping,
+  elapsedTime,
+  endStyles,
+  stiffness,
+}) => {
+  return Object.keys(currentStyles).reduce((result, styleName) => {
+    const currentStyle = currentStyles[styleName];
+    const endStyle = endStyles[styleName];
+    const parsedStartStyle = parseStyle(currentStyle);
+    const parsedEndStyle = parseStyle(endStyle);
+
+    const currentVelocity = currentVelocities[styleName];
+
+    const spring = -stiffness * (parsedStartStyle.value - parsedEndStyle.value);
+    const damper = -damping * currentVelocity;
+    const acceleration = spring + damper;
+    const updatedVelocity = currentVelocity + (acceleration * SECONDS_PER_ANIMATION_FRAME);
+    const updatedStyle = updateStyleValue(
+      parsedStartStyle,
+      updatedVelocity * SECONDS_PER_ANIMATION_FRAME
+    );
+    const reconstructedStyle = reconstructStyle(updatedStyle);
+
+    result.updatedStyles[styleName] = reconstructedStyle;
+    result.updatedVelocities[styleName] = updatedVelocity;
+
+    rigRef.style[styleName] = reconstructedStyle;
+
+    return result;
+  }, { updatedStyles: {}, updatedVelocities: {} });
+}
+
+const allVelocitiesSettled = currentStylesAndVelocities => {
+  const velocityChecks = Object.keys(currentStylesAndVelocities)
+    .reduce((result, rigName) => {
+      const { currentVelocities } = currentStylesAndVelocities[rigName];
+      return result.concat(
+        Object.values(currentVelocities)
+          .map(velocity => Math.abs(velocity) < 0.01)
+      );
+    }, []);
+  return velocityChecks.every(check => check);
+}
+
 const createStyleUpdater = ({
   allEndStyles,
   allStartStyles,
@@ -152,30 +197,6 @@ const createStyleUpdater = ({
   easingFn,
   rigs,
 }) => elapsedTime => {
-  Object.keys(rigs).forEach(rigName => {
-    const startStyles = allStartStyles[rigName];
-    const endStyles = allEndStyles[rigName];
-    const rigRef = rigs[rigName];
-
-    if (!rigRef) {
-      // TODO: warn
-      return;
-    }
-
-    if (!startStyles || !endStyles) {
-      // TODO: warn
-      return;
-    }
-
-    updateRigStyles({
-      rigRef: rigs[rigName],
-      startStyles,
-      endStyles,
-      easingFn,
-      duration,
-      elapsedTime,
-    });
-  });
 }
 
 const runAnimationStageWithoutStyleUpdates = ({
@@ -203,7 +224,7 @@ const runAnimationStageWithoutStyleUpdates = ({
   currentFrame = requestAnimationFrame(runNextAnimationFrame);
 }
 
-const runAnimationStage = ({
+const runTimedAnimationStage = ({
   animationStage,
   cancelAnimationFrame,
   runNextStage,
@@ -220,27 +241,121 @@ const runAnimationStage = ({
   const startTime = Date.now();
   let currentFrame;
 
-  const updateStyles = createStyleUpdater({
-    allStartStyles,
-    allEndStyles,
-    rigs,
-    easingFn,
-    duration,
-  });
+  const updateTimedStyles = elapsedTime => {
+    Object.keys(rigs).forEach(rigName => {
+      const startStyles = allStartStyles[rigName];
+      const endStyles = allEndStyles[rigName];
+      const rigRef = rigs[rigName];
+
+      if (!rigRef) {
+        // TODO: warn
+        return;
+      }
+
+      if (!startStyles || !endStyles) {
+        // TODO: warn
+        return;
+      }
+
+      updateTimedRigStyles({
+        rigRef: rigs[rigName],
+        startStyles,
+        endStyles,
+        easingFn,
+        duration,
+        elapsedTime,
+      });
+    });
+  };
 
   const runLastAnimationFrame = () => {
     currentFrame = null;
     if (duration > 0) {
-      updateStyles(duration);
+      updateTimedStyles(duration);
     }
     runNextStage();
   };
 
   const runNextAnimationFrame = () => {
     const elapsedTime = Date.now() - startTime;
-    updateStyles(elapsedTime);
+    updateTimedStyles(elapsedTime);
 
     if (elapsedTime < duration) {
+      currentFrame = requestAnimationFrame(runNextAnimationFrame);
+    } else {
+      currentFrame = requestAnimationFrame(runLastAnimationFrame);
+    }
+  };
+  currentFrame = requestAnimationFrame(runNextAnimationFrame);
+}
+
+const runSpringAnimationStage = ({
+  animationStage,
+  cancelAnimationFrame,
+  runNextStage,
+  requestAnimationFrame,
+  rigs,
+}) => {
+  const {
+    start: allStartStyles,
+    end: allEndStyles,
+    stiffness,
+    damping,
+  } = animationStage;
+
+  const startTime = Date.now();
+  const currentStylesAndVelocities = Object.keys(allStartStyles).reduce((result, rigName) => {
+    const currentStyles = allStartStyles[rigName];
+    result[rigName] = {
+      currentStyles,
+      currentVelocities: Object.keys(currentStyles).reduce((velocities, styleName) => {
+        velocities[styleName] = 0;
+        return velocities;
+      }, {}),
+    };
+    return result;
+  }, {});
+  let currentFrame;
+
+  const updateSpringStyles = elapsedTime => {
+    Object.keys(allStartStyles).forEach(rigName => {
+      const { currentStyles, currentVelocities } = currentStylesAndVelocities[rigName];
+      const endStyles = allEndStyles[rigName];
+      const rigRef = rigs[rigName];
+
+      if (!rigRef) {
+        // TODO: warn
+        return;
+      }
+
+      if (!currentStyles || !endStyles) {
+        // TODO: warn
+        return;
+      }
+
+      const { updatedStyles, updatedVelocities } = updateSpringRigStyles({
+        rigRef: rigs[rigName],
+        currentStyles,
+        currentVelocities,
+        damping,
+        elapsedTime,
+        endStyles,
+        stiffness,
+      });
+      currentStylesAndVelocities[rigName].currentStyles = updatedStyles;
+      currentStylesAndVelocities[rigName].currentVelocities = updatedVelocities;
+    });
+  };
+
+  const runLastAnimationFrame = () => {
+    currentFrame = null;
+    runNextStage();
+  };
+
+  const runNextAnimationFrame = () => {
+    const elapsedTime = Date.now() - startTime;
+    updateSpringStyles(elapsedTime);
+    if (!allVelocitiesSettled(currentStylesAndVelocities)) {
       currentFrame = requestAnimationFrame(runNextAnimationFrame);
     } else {
       currentFrame = requestAnimationFrame(runLastAnimationFrame);
@@ -263,9 +378,18 @@ const runAnimation = ({
       start,
       end,
       duration,
+      stiffness,
+      damping,
       onStageComplete,
     } = animationStage;
+
     const hasStyleUpdates = !!start && !!end;
+    const isUsingTime = duration != null;
+    const isUsingSpring = stiffness != null && damping != null;
+
+    if (isUsingTime && isUsingSpring) {
+      // TODO: console.warn
+    }
 
     const runNextStage = () => {
       const nextStageNum = currentStageNum + 1;
@@ -284,13 +408,23 @@ const runAnimation = ({
     }
 
     if (hasStyleUpdates) {
-      runAnimationStage({
-        animationStage,
-        cancelAnimationFrame,
-        requestAnimationFrame,
-        rigs,
-        runNextStage,
-      });
+      if (isUsingTime) {
+        runTimedAnimationStage({
+          animationStage,
+          cancelAnimationFrame,
+          requestAnimationFrame,
+          rigs,
+          runNextStage,
+        });
+      } else if (isUsingSpring) {
+        runSpringAnimationStage({
+          animationStage,
+          cancelAnimationFrame,
+          requestAnimationFrame,
+          rigs,
+          runNextStage,
+        });
+      }
     } else {
       runAnimationStageWithoutStyleUpdates({
         cancelAnimationFrame,
