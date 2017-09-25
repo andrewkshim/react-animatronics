176642450a77300974760cd995d65a04092adf5e
Andrew Shim <andrewkshim92@gmail.com>
2017-09-24 19:46:50 -0700

    📚: Added springs example

---
+++ examples/springs/src/index.js
@@ -0,0 +1,135 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import PropTypes from 'prop-types'
+
+import { withAnimatronics, withRig } from '../../../src'
+
+class Circle extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const { dimension, style } = this.props;
+    return (
+      <div
+        style={{
+          width: `${ dimension }px`,
+          height: `${ dimension }px`,
+          borderRadius: `${ dimension / 2 }px`,
+          ...style,
+        }}
+      />
+    );
+  }
+}
+
+const BlueCircle = withRig('blueCircle')(Circle);
+const RedCircle = withRig('redCircle')(Circle);
+const PurpleCircle = withRig('purpleCircle')(Circle);
+
+const App = ({
+  runAnimation,
+}) => (
+  <div style={{
+    width: '100%',
+    height: '100%',
+    display: 'flex',
+  }}>
+    <div style={{
+      margin: '0 auto',
+      width: '70%',
+      position: 'relative',
+      display: 'flex',
+      flexDirection: 'column',
+    }}>
+      <button
+        onClick={ () => runAnimation() }
+        style={{
+          margin: '20px 0px',
+          width: '200px',
+        }}
+      >
+        Animate!
+      </button>
+      <BlueCircle
+        dimension={ 80 }
+        style={{
+          backgroundColor: 'blue',
+          margin: '20px 0px',
+          position: 'relative',
+        }}
+      />
+      <RedCircle
+        dimension={ 80 }
+        style={{
+          backgroundColor: 'red',
+          margin: '20px 0px',
+          position: 'relative',
+        }}
+      />
+      <PurpleCircle
+        dimension={ 80 }
+        style={{
+          backgroundColor: 'purple',
+          margin: '20px 0px',
+          position: 'relative',
+        }}
+      />
+    </div>
+  </div>
+);
+
+const AnimatedApp = withAnimatronics(
+  ({ blueCircle, redCircle, purpleCircle }) => {
+    return [
+      {
+        stiffness: 170,
+        damping: 26,
+        start: {
+          blueCircle: {
+            left: '0px',
+          },
+        },
+        end: {
+          blueCircle: {
+            left: '400px',
+          },
+        },
+      },
+      {
+        stiffness: 100,
+        damping: 10,
+        start: {
+          redCircle: {
+            left: '0px',
+          },
+        },
+        end: {
+          redCircle: {
+            left: '400px',
+          },
+        },
+      },
+      {
+        stiffness: 200,
+        damping: 5,
+        start: {
+          purpleCircle: {
+            left: '0px',
+          },
+        },
+        end: {
+          purpleCircle: {
+            left: '400px',
+          },
+        },
+      }
+    ];
+  }
+)( App );
+
+ReactDOM.render(
+  <AnimatedApp />,
+  document.getElementById('app'),
+);
