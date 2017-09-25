02ff9ca608cb358bd49e17d8c50f5ecd17f80630
Andrew Shim <andrewkshim92@gmail.com>
2017-09-24 19:43:27 -0700

    ✨: Moved all style-related logic into own module
    - And shored up the spring logic by copying react-motion

--- src/index.js
+++ src/index.js
@@ -4,7 +4,15 @@ import PropTypes from 'prop-types'
 import BezierEasing from 'bezier-easing'
 import chroma from 'chroma-js'
 
-import CSSColors from './css-colors'
+import {
+  MS_PER_ANIMATION_FRAME,
+  SECONDS_PER_ANIMATION_FRAME,
+} from './constants'
+
+import {
+  updateTimedRigStyles,
+  createFnUpdateSpringRigStyles,
+} from './stylist'
 
 //==========================================================
 // Constants
@@ -14,14 +22,10 @@ const ACTION_PREFIX = '@@animatronics';
 const REGISTER_COMPONENT = `${ ACTION_PREFIX }/REGISTER_COMPONENT`;
 const UNREGISTER_COMPONENT = `${ ACTION_PREFIX }/UNREGISTER_COMPONENT`;
 
-const CSS_TRANSFORM = 'transform';
-
 const IS_RAF_AVAILABLE = (
   typeof window !== 'undefined'
   && window.requestAnimationFrame
 );
-const MS_PER_ANIMATION_FRAME = 1000 / 60; // 60 fps
-const SECONDS_PER_ANIMATION_FRAME = MS_PER_ANIMATION_FRAME / 1000;
 const DEFAULT_EASING_FN = BezierEasing(0.4, 0.0, 0.2, 1);
 
 const DEFAULT_REQUEST_ANIMATION_FRAME = IS_RAF_AVAILABLE
@@ -40,64 +44,11 @@ const NUMBER_REGEX = /(-)?\d+(\.\d+)?/;
 // Internal
 //==========================================================
 
+const ensureIsFunction = possibleFn => typeof possibleFn === 'function'
+  ? possibleFn
+  : () => {};
+
 const isStatelessComponent = Component => !Component.prototype.render;
-
-// TODO: use styleName?
-const parseStyle = (styleName, style) => {
-  if (typeof style === 'number') {
-    return {
-      value: style,
-    };
-  } else if (style.indexOf('(') > -1) {
-    const transforms = style.split(' ');
-    return transforms.reduce((result, transform) => {
-      const openParenIndex = transform.indexOf('(');
-      const matches = BETWEEN_PAREN_REGEX.exec(transform);
-      result.transformFns.push(transform.slice(0, openParenIndex));
-      result.transformStyles.push(parseStyle(styleName, matches[1]));
-      return result;
-    }, { transformFns: [], transformStyles: [] });
-  } else if (!!CSSColors[style]) {
-    return {
-      isColor: true, // TODO: type the styles?
-      value: CSSColors[style],
-    };
-  } else {
-    const matches = NUMBER_REGEX.exec(style);
-    const value = matches[0];
-    return {
-      value: parseFloat(value),
-      unit: style.slice(value.length),
-    };
-  }
-}
-
-const updateStyleValue = (parsedStyle, value) => {
-  return {
-    ...parsedStyle,
-    value,
-  };
-}
-
-const updateTransformStyles = ({}) => {
-  return {
-  };
-}
-
-const stringifyStyle = ({ transformFns, transformStyles, value, unit }) => {
-  return transformFns ?
-    transformFns
-      .reduce((styleBuilder, transformFn, index) => {
-        const { value, unit } = transformStyles[index];
-        return styleBuilder.concat(`${transformFn}(${value}${unit})`);
-      }, [])
-      .join(' ')
-  : !unit ?
-    value
-  :
-    `${value}${unit}`
-  ;
-}
 
 const ANIMATRONICS_ACTION_HANDLERS = {
 
@@ -131,139 +82,6 @@ const handleAnimatronicsAction = (state, { type, payload }) => {
     ? state
     : ANIMATRONICS_ACTION_HANDLERS[type](state, payload)
   ;
-}
-
-const isColorStyle = parsedStyle => parsedStyle.isColor;
-const isTransformStyle = parsedStyle => parsedStyle.transformFns;
-
-const calculateTimedUpdatedStyle = ({
-  startValue,
-  endValue,
-  easingFn,
-  duration,
-  elapsedTime,
-  styleName,
-}) => {
-  const normalizedDuration = duration === 0 ? elapsedTime : duration;
-  const parsedStartStyle = parseStyle(styleName, startValue);
-  const parsedEndStyle = parseStyle(styleName, endValue);
-  if (isColorStyle(parsedStartStyle)) {
-    if (!isColorStyle(parsedEndStyle)) {
-      console.error('using different styles D:');
-    }
-  }
-  return stringifyStyle(
-    isColorStyle(parsedStartStyle) ?
-      updateStyleValue(
-        parsedStartStyle,
-        chroma.mix(
-          parsedStartStyle.value,
-          parsedEndStyle.value,
-          easingFn( elapsedTime / normalizedDuration ),
-        )
-      )
-    : isTransformStyle(parsedStartStyle) ?
-      {
-        ...parsedStartStyle,
-        transformStyles: parsedStartStyle.transformStyles.map(
-          (parsedStyle, index) => {
-            return updateStyleValue(
-              parsedStyle,
-              parsedStyle.value + (
-                (parsedEndStyle.transformStyles[index].value - parsedStyle.value) * easingFn( elapsedTime / normalizedDuration )
-              )
-            )
-          }
-        ),
-      }
-    :
-      updateStyleValue(
-        parsedStartStyle,
-        parsedStartStyle.value + (
-          (parsedEndStyle.value - parsedStartStyle.value) * easingFn( elapsedTime / normalizedDuration )
-        ),
-      )
-  );
-}
-
-const updateTimedRigStyles = ({
-  rigRef,
-  startStyles,
-  endStyles,
-  easingFn,
-  duration,
-  elapsedTime,
-}) => {
-  Object.keys(startStyles).forEach(styleName => {
-    const updatedStyle = calculateTimedUpdatedStyle({
-      startValue: startStyles[styleName],
-      endValue: endStyles[styleName],
-      easingFn,
-      duration,
-      elapsedTime,
-      styleName,
-    });
-    rigRef.style[styleName] = updatedStyle;
-  });
-}
-
-const updateSpringRigStyles = ({
-  rigRef,
-  currentStyles,
-  currentVelocities,
-  damping,
-  elapsedTime,
-  endStyles,
-  stiffness,
-}) => {
-  return Object.keys(currentStyles).reduce((result, styleName) => {
-    const currentStyle = currentStyles[styleName];
-    const endStyle = endStyles[styleName];
-    const parsedStartStyle = parseStyle(styleName, currentStyle);
-    const parsedEndStyle = parseStyle(styleName, endStyle);
-
-    const currentVelocity = currentVelocities[styleName];
-
-    const spring = -stiffness * (parsedStartStyle.value - parsedEndStyle.value);
-    const damper = -damping * currentVelocity;
-    const acceleration = spring + damper;
-    const updatedVelocity = currentVelocity + (acceleration * SECONDS_PER_ANIMATION_FRAME);
-    const updatedStyle = updateStyleValue(
-      parsedStartStyle,
-      parsedStartStyle.value + (
-        updatedVelocity * SECONDS_PER_ANIMATION_FRAME
-      )
-    );
-    const reconstructedStyle = stringifyStyle(updatedStyle);
-
-    result.updatedStyles[styleName] = reconstructedStyle;
-    result.updatedVelocities[styleName] = updatedVelocity;
-
-    rigRef.style[styleName] = reconstructedStyle;
-
-    return result;
-  }, { updatedStyles: {}, updatedVelocities: {} });
-}
-
-const allVelocitiesSettled = currentStylesAndVelocities => {
-  const velocityChecks = Object.keys(currentStylesAndVelocities)
-    .reduce((result, rigName) => {
-      const { currentVelocities } = currentStylesAndVelocities[rigName];
-      return result.concat(
-        Object.values(currentVelocities)
-          .map(velocity => Math.abs(velocity) < 0.01)
-      );
-    }, []);
-  return velocityChecks.every(check => check);
-}
-
-const createStyleUpdater = ({
-  allEndStyles,
-  allStartStyles,
-  duration,
-  easingFn,
-  rigs,
-}) => elapsedTime => {
 }
 
 const runAnimationStageWithoutStyleUpdates = ({
@@ -370,69 +188,35 @@ const runSpringAnimationStage = ({
     damping,
   } = animationStage;
 
-  const startTime = Date.now();
-  const currentStylesAndVelocities = Object.keys(allStartStyles).reduce((result, rigName) => {
-    const currentStyles = allStartStyles[rigName];
-    result[rigName] = {
-      currentStyles,
-      currentVelocities: Object.keys(currentStyles).reduce((velocities, styleName) => {
-        velocities[styleName] = 0;
-        return velocities;
-      }, {}),
-    };
-    return result;
-  }, {});
+  const updateSpringRigStyles = createFnUpdateSpringRigStyles({
+    allStartStyles,
+    allEndStyles,
+    stiffness,
+    damping,
+  });
+
   let currentFrame;
+  let isAnimationDone = false;
 
-  const updateSpringStyles = elapsedTime => {
+  const updateSpringStyles = () => {
     Object.keys(allStartStyles).forEach(rigName => {
-      const { currentStyles, currentVelocities } = currentStylesAndVelocities[rigName];
-      const endStyles = allEndStyles[rigName];
-      const rigRef = rigs[rigName];
-
-      if (!rigRef) {
-        // TODO: warn
-        return;
-      }
-
-      if (!currentStyles || !endStyles) {
-        // TODO: warn
-        return;
-      }
-
-      const { updatedStyles, updatedVelocities } = updateSpringRigStyles({
+      isAnimationDone = updateSpringRigStyles({
         rigRef: rigs[rigName],
-        currentStyles,
-        currentVelocities,
-        damping,
-        elapsedTime,
-        endStyles,
-        stiffness,
+        rigName,
+        styleNames: Object.keys(allStartStyles[rigName]),
       });
-      console.log(currentStyles, updatedStyles);
-      currentStylesAndVelocities[rigName].currentStyles = updatedStyles;
-      currentStylesAndVelocities[rigName].currentVelocities = updatedVelocities;
     });
   };
 
   const runLastAnimationFrame = () => {
     currentFrame = null;
-    Object.keys(allStartStyles).forEach(rigName => {
-      const endStyles = allEndStyles[rigName];
-      const rigRef = rigs[rigName];
-      Object.keys(endStyles).forEach(styleName => {
-        rigRef.style[styleName] = stringifyStyle(
-          parseStyle(styleName, endStyles[styleName])
-        );
-      });
-    });
+    updateSpringStyles();
     runNextStage();
   };
 
   const runNextAnimationFrame = () => {
-    const elapsedTime = Date.now() - startTime;
-    updateSpringStyles(elapsedTime);
-    if (!allVelocitiesSettled(currentStylesAndVelocities)) {
+    updateSpringStyles();
+    if (!isAnimationDone) {
       currentFrame = requestAnimationFrame(runNextAnimationFrame);
     } else {
       currentFrame = requestAnimationFrame(runLastAnimationFrame);
@@ -586,8 +370,8 @@ export const withAnimatronics = (
         runAnimation({
           animationStages: createAnimationStages(rigs),
           cancelAnimationFrame,
-          onAnimationComplete,
-          onStageComplete,
+          onAnimationComplete: ensureIsFunction(onAnimationComplete),
+          onStageComplete: ensureIsFunction(onStageComplete),
           requestAnimationFrame,
           rigs,
         })
---
+++ src/utils.js
@@ -0,0 +1,4 @@
+
+const MODULE_PREFIX = '@@animatronics';
+
+export const createModuleString = str => `${ MODULE_PREFIX }/${ str }`;
