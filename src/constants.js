72decc0582e802def291d9278b049d8697d1c749
Andrew Shim <andrewkshim92@gmail.com>
2017-09-24 19:47:13 -0700

    🐛: Added missing constants module

---
+++ src/constants.js
@@ -0,0 +1,9 @@
+/**
+ * Constants: where any shared constants go.
+ * @module constants
+ */
+
+// 60 fps
+export const MS_PER_ANIMATION_FRAME = 1000 / 60;
+
+export const SECONDS_PER_ANIMATION_FRAME = MS_PER_ANIMATION_FRAME / 1000;
