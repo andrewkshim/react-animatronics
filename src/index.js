916375b6d76529935caf0e6b35fca59889551919
Andrew Shim <andrewkshim92@gmail.com>
2017-10-01 18:24:01 -0700

    ♻: export api from index

--- src/index.js
+++ src/index.js
@@ -2,3 +2,6 @@
  * React Animatronics: coordinated, declarative animations for React components.
  */
 
+export withAnimatronics from './withAnimatronics'
+export withRig from './withRig'
+export createBezierEasingFn from './createBezierEasingFn'
