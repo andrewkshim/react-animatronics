6c103df95d8f7fe73a8a3b6897d0d558a53f4fd9
Andrew Shim <andrewkshim92@gmail.com>
2017-10-07 15:43:06 -0700

    :white_check_mark: Added tests for machine

---
+++ src/internal/machine.spec.js
@@ -0,0 +1,56 @@
+// @flow
+import sinon from 'sinon'
+import test from 'tape'
+
+import { PerpetualMachineFactory, TimedMachineFactory } from './machine'
+
+test('PerpetualMachineFactory', assert => {
+  const expectedCallCount = 5;
+  const interval = 100;
+  const requestAnimationFrame = (fn) => { setTimeout(fn, interval) };
+  const cancelAnimationFrame = clearTimeout;
+  const spy = sinon.spy();
+  let numIterations = 0;
+
+  const onComplete = () => {
+    assert.equals(spy.callCount, expectedCallCount);
+    assert.end();
+  }
+
+  const factory = PerpetualMachineFactory(requestAnimationFrame, cancelAnimationFrame);
+  const machine = factory(onComplete);
+
+  const job = () => {
+    if (numIterations === expectedCallCount) {
+      machine.stop();
+    }
+    numIterations++;
+    spy();
+  };
+
+  machine
+    .do(job)
+    .run();
+});
+
+test('TimedMachineFactory', assert => {
+  const duration = 500;
+  const interval = 100;
+  const expectedCallCount = Math.floor(duration / interval);
+  const requestAnimationFrame = (fn) => { setTimeout(fn, interval) };
+  const cancelAnimationFrame = clearTimeout;
+  const job = sinon.spy();
+
+  const onComplete = () => {
+    assert.equals(job.callCount, expectedCallCount, 'the machine runs the expected number of iterations');
+    assert.true(machine.isStopped(), 'the machine knows when it is stopped');
+    assert.end();
+  }
+
+  const factory = TimedMachineFactory(requestAnimationFrame, cancelAnimationFrame);
+  const machine = factory(duration, onComplete);
+
+  machine
+    .do(job)
+    .run();
+});
