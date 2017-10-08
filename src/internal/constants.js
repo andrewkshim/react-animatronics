bf49758c6f69b370a5b9a0eba54e367a40ce9c38
Andrew Shim <andrewkshim92@gmail.com>
2017-10-08 11:39:40 -0700

    :art: Added flow types to "constants" module

--- src/internal/constants.js
+++ src/internal/constants.js
@@ -1,11 +1,12 @@
+// @flow
 /**
  * Constants: where any shared constants go.
  * @module constants
  */
 
-const MS_PER_ANIMATION_FRAME = 1000 / 60;
+const MS_PER_ANIMATION_FRAME: number = 1000 / 60;
 
-const SECONDS_PER_ANIMATION_FRAME = MS_PER_ANIMATION_FRAME / 1000;
+const SECONDS_PER_ANIMATION_FRAME: number = MS_PER_ANIMATION_FRAME / 1000;
 
 const Constants = {
   MS_PER_ANIMATION_FRAME,
