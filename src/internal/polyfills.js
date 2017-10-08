546f661f4f839e084e1d97b5ec0721e96ab423b7
Andrew Shim <andrewkshim92@gmail.com>
2017-10-08 13:46:00 -0700

    :art: Added flow types to polyfills module

--- src/internal/polyfills.js
+++ src/internal/polyfills.js
@@ -1,3 +1,4 @@
+// @flow
 /**
  * polyfills: provides anything related to the browser environment
  * @module internal/polyfills
@@ -5,18 +6,18 @@
 
 import Constants from './constants'
 
-const IS_RAF_AVAILABLE = (
+const IS_RAF_AVAILABLE: boolean = (
   typeof window !== 'undefined'
   && window.requestAnimationFrame
 );
 
-const DEFAULT_REQUEST_ANIMATION_FRAME = (
+const DEFAULT_REQUEST_ANIMATION_FRAME: Function = (
   IS_RAF_AVAILABLE
     ? requestAnimationFrame
     : callback => setTimeout(callback, Constants.MS_PER_ANIMATION_FRAME)
 );
 
-const DEFAULT_CANCEL_ANIMATION_FRAME = (
+const DEFAULT_CANCEL_ANIMATION_FRAME: Function = (
   IS_RAF_AVAILABLE
     ? cancelAnimationFrame
     : clearTimeout
