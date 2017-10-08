6837e4a6bb22509cfc79ffc222e325105c5bec6c
Andrew Shim <andrewkshim92@gmail.com>
2017-10-08 11:39:24 -0700

    :art: Added semicolons at end of exports

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@
  * React Animatronics: coordinated, declarative animations for React components.
  */
 
-export withAnimatronics from './withAnimatronics'
-export withRig from './withRig'
-export createBezierEasingFn from './createBezierEasingFn'
+export withAnimatronics from './withAnimatronics';
+export withRig from './withRig';
+export createBezierEasingFn from './createBezierEasingFn';
