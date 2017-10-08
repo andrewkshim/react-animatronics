2d4d1132586995e24d84473db60acaea191aaccb
Andrew Shim <andrewkshim92@gmail.com>
2017-10-08 12:50:16 -0700

    :sparkles: Keep it simple, don't rename BezierEasing

--- src/createBezierEasingFn.js
+++ src/createBezierEasingFn.js
@@ -1,9 +1,3 @@
-/**
- * createBezierEasingFn: (x1, y1, x2, y2) => bezier easing fn
- */
-
 import BezierEasing from 'bezier-easing'
 
-const createBezierEasingFn = BezierEasing;
-
-export default createBezierEasingFn;
+export default BezierEasing;
