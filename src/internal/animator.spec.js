82c5ec0a82c543e9bb709f186bd6c1cc500df875
Andrew Shim <andrewkshim92@gmail.com>
2017-10-09 18:53:49 -0700

    :white_check_mark: Added animator test for multiple stages

--- src/internal/animator.spec.js
+++ src/internal/animator.spec.js
@@ -32,7 +32,7 @@ test('reverseStages', assert => {
   assert.end();
 });
 
-test('playAnimation', assert => {
+test('playAnimation with a single stage', assert => {
   const interval = 100;
   const duration = 500;
   const stages = [
@@ -63,6 +63,54 @@ test('playAnimation', assert => {
       );
       assert.deepEquals(
         styleUpdater.lastCall.args[0], { left: '100px' },
+        'provides the style updater with the correct end styles'
+      );
+      assert.end();
+    },
+  );
+});
+
+test('playAnimation with multiple stages', { timeout: 1000 }, assert => {
+  const interval = 100;
+  const duration = 200;
+  const stages = [
+    {
+      componentA: {
+        duration,
+        start: { left: '0px' },
+        end: { left: '100px' },
+      },
+    },
+    {
+      componentA: {
+        duration,
+        start: { left: '100px' },
+        end: { left: '200px' },
+      },
+    },
+    {
+      componentA: {
+        duration,
+        start: { left: '200px' },
+        end: { left: '300px' },
+      },
+    },
+  ];
+  const requestAnimationFrame = fn => { setTimeout(fn, interval) };
+  const cancelAnimationFrame = clearTimeout;
+  const controls = ControlsMachine();
+  const animation = AnimationMachine(requestAnimationFrame, cancelAnimationFrame);
+  const styleUpdater = sinon.spy();
+
+  controls.registerComponent('componentA', {}, styleUpdater);
+
+  playAnimation(
+    stages,
+    controls,
+    animation,
+    () => {
+      assert.deepEquals(
+        styleUpdater.lastCall.args[0], { left: '300px' },
         'provides the style updater with the correct end styles'
       );
       assert.end();
