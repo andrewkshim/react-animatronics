8f63bc77ce592d431f35202c6a70b483e835e810
Andrew Shim <andrewkshim92@gmail.com>
2017-10-14 14:58:36 -0700

    :white_check_mark: Updated setup.test to use new enzyme API

--- src/internal/setup.test.js
+++ src/internal/setup.test.js
@@ -1,4 +1,8 @@
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import { JSDOM } from 'jsdom';
+
+Enzyme.configure({ adapter: new Adapter() });
 
 const dom = new JSDOM('<html><body></body></html>');
 global.window = dom.window;
