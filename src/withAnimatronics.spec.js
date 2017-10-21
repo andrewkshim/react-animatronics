25cc3e7f64a2e1cddc7834e740874babbc05b0ad
Andrew Shim <andrewkshim92@gmail.com>
2017-10-21 12:54:00 -0700

    :white_check_mark: [withAnimatronics] Added test for error

--- src/withAnimatronics.spec.js
+++ src/withAnimatronics.spec.js
@@ -5,17 +5,49 @@ import { mount } from 'enzyme'
 import withAnimatronics from './withAnimatronics'
 
 test('withAnimatronics creates a valid React element', assert => {
-  const Animated = withAnimatronics()(() => <div/>);
+  const createAnimationSequences = () => [
+    {
+      base: {
+        duration: 100,
+        start: { left: '100px' },
+        end: { left: '200px' },
+      }
+    }
+  ];
+
+  const Animated = withAnimatronics(createAnimationSequences)(() => <div/>);
+
   const element = React.createElement(Animated);
   assert.true(React.isValidElement(element));
   assert.end();
 });
 
-test('withAnimatronics supplies playAnimation() to the wrapped component', assert => {
+test('withAnimatronics.playAnimation', assert => {
   const Base = () => <div/>;
-  const Animated = withAnimatronics()(Base);
+  const createAnimationSequences = () => [
+    {
+      base: {
+        duration: 100,
+        start: { left: '100px' },
+        end: { left: '200px' },
+      }
+    }
+  ];
+
+  const Animated = withAnimatronics(createAnimationSequences)(Base);
   const wrapper = mount(<Animated/>);
   const playAnimation = wrapper.find(Base).prop('playAnimation');
-  assert.true(typeof playAnimation === 'function');
+
+  assert.true(
+    typeof playAnimation === 'function',
+    'supplies playAnimation() to the wrapped component'
+  );
+
+  assert.throws(
+    () => playAnimation({}),
+    /expects its first argument to be the string name of your animation/,
+    'playAnimation() correctly throws when provided incorrect arguments',
+  );
+
   assert.end();
 });
