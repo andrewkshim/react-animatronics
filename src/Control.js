1af6ff713f27d70baab1879e6f10c7e577978ce2
Andrew Shim <andrewkshim92@gmail.com>
2017-10-21 08:54:43 -0700

    :rewind: [Control] Undo moving the component into Control
    Keep withControl as the thing that defines the component and define Control in terms of withControl.

--- src/Control.js
+++ src/Control.js
@@ -3,59 +3,20 @@
  */
 
 import React from 'react'
-import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
 
 import ContextTypes from './internal/context-types'
+import withControl from './withControl'
 
 class Control extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { style: {} };
-    this._onRef = this._onRef.bind(this);
-    this._setComponentStyle = this._setComponentStyle.bind(this);
-  }
-
-  componentDidMount() {
-    const { animatronics } = this.context;
-    const { name, useStringRefs } = this.props;
-    const ref = useStringRefs ? this.refs[name] : this._ref;
-    const domNode = ReactDOM.findDOMNode(ref);
-    animatronics.registerComponent(
-      name,
-      domNode,
-      this._setComponentStyle,
-    );
-  }
-
-  componentWillUnmount() {
-    const { animatronics } = this.context;
-    const { name } = this.props;
-    animatronics.unregisterComponent(name);
-  }
-
-  _setComponentStyle(updatedStyles) {
-    this.setState(state => ({
-      style: {
-        ...state.style,
-        ...updatedStyles,
-      },
-    }));
-  }
-
-  _onRef(ref) {
-    this._ref = ref;
-  }
-
   render() {
-    const { children, name, useStringRefs, ...props } = this.props;
-    const { style } = this.state;
-    const ref = useStringRefs ? name : this._onRef;
-    return React.cloneElement(children, {
-      animatronicStyles: style,
-      ref,
-      ...props,
-    });
+    const { name, children, useStringRefs } = this.props;
+    const enhance = withControl(name, { useStringRefs });
+    class BaseComponent extends React.Component {
+      render() { return this.props.children; }
+    }
+    const ControlledComponent = enhance(BaseComponent);
+    return <ControlledComponent children={ children }/>;
   }
 }
 
--- src/withControl.js
+++ src/withControl.js
@@ -1,27 +1,69 @@
 /**
+ * TODO: improve docs
  * withControl: (name, options) => higher-order component
  * @module internal/context-types
  */
 
 import React from 'react'
+import ReactDOM from 'react-dom'
 
 import ContextTypes from './internal/context-types'
-import Control from './Control'
 
 const withControl = (
   name,
   { useStringRefs = false } = {}
 ) => BaseComponent => {
 
-  const ControlledComponent = class extends React.Component {
-    render() {
-      return (
-        <Control name={ name } useStringRefs={ useStringRefs }>
-          <BaseComponent { ...this.props }/>
-        </Control>
+  class ControlledComponent extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = { style: {} };
+      this._onRef = this._onRef.bind(this);
+      this._setComponentStyle = this._setComponentStyle.bind(this);
+    }
+
+    componentDidMount() {
+      const { animatronics } = this.context;
+      const ref = useStringRefs ? this.refs[name] : this._ref;
+      const domNode = ReactDOM.findDOMNode(ref);
+      animatronics.registerComponent(
+        name,
+        domNode,
+        this._setComponentStyle,
       );
     }
-  };
+
+    componentWillUnmount() {
+      const { animatronics } = this.context;
+      animatronics.unregisterComponent(name);
+    }
+
+    _setComponentStyle(updatedStyles) {
+      this.setState(state => ({
+        style: {
+          ...state.style,
+          ...updatedStyles,
+        },
+      }));
+    }
+
+    _onRef(ref) {
+      this._ref = ref;
+    }
+
+    render() {
+      const { ...props } = this.props;
+      const { style } = this.state;
+      const ref = useStringRefs ? name : this._onRef;
+      return (
+        <BaseComponent
+          ref={ ref }
+          animatronicStyles={ style }
+          { ...props }
+        />
+      );
+    }
+  }
 
   ControlledComponent.contextTypes = ContextTypes;
 
