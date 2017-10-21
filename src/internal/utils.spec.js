ae7694f11078c4d0fa92ccc47620707b9035bd0c
Andrew Shim <andrewkshim92@gmail.com>
2017-10-21 13:14:27 -0700

    :white_check_mark: [utils] Fix makeError test

--- src/internal/utils.spec.js
+++ src/internal/utils.spec.js
@@ -6,7 +6,7 @@ import { makeError } from './utils'
 test('utils.makeError', assert => {
   const err = makeError('hello', 'world');
   assert.equals(
-    err.message, 'hello\nworld',
+    err.message, 'hello world',
     'correctly formats the error message'
   );
   assert.end();
