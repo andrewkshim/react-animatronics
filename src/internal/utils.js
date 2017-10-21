cd1178f0e13d48d2c5d63b7350c1cfb7c2a95b3b
Andrew Shim <andrewkshim92@gmail.com>
2017-10-21 09:42:05 -0700

    :sparkles: [utils] Added isStatelessComponent helper fn

--- src/internal/utils.js
+++ src/internal/utils.js
@@ -10,3 +10,5 @@ const PACKAGE_PREFIX: string = '@@animatronics';
 export const createPackageString = (str: string): string => `${ PACKAGE_PREFIX }/${ str }`;
 
 export const noop = (): void => {};
+
+export const isStatelessComponent = Component => !Component.prototype.render;
