fc73a822f7b0ed4dca6c82254445f8050e319bd8
Andrew Shim <andrewkshim92@gmail.com>
2017-10-21 00:08:54 -0700

    :sparkles: [index] Export Control

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 /**
  * React Animatronics: coordinated, declarative animations for React components.
  */
+export Control from './Control'
 export withAnimatronics from './withAnimatronics'
 export withControl from './withControl'
 export BezierEasing from 'bezier-easing'
