05da8b85442d8af835fc8d53a7c376016b6cb75e
Andrew Shim <andrewkshim92@gmail.com>
2017-10-21 00:10:07 -0700

    :art: [index] Alphabetize exports

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 /**
  * React Animatronics: coordinated, declarative animations for React components.
  */
+export BezierEasing from 'bezier-easing'
 export Control from './Control'
 export withAnimatronics from './withAnimatronics'
 export withControl from './withControl'
-export BezierEasing from 'bezier-easing'
