a221f049b1bc418cccf7b611b71ec25de30359a9
Andrew Shim <andrewkshim92@gmail.com>
2017-10-21 09:39:07 -0700

    :fire: [utils] Remove old, unused method

--- src/internal/utils.js
+++ src/internal/utils.js
@@ -10,9 +10,3 @@ const PACKAGE_PREFIX: string = '@@animatronics';
 export const createPackageString = (str: string): string => `${ PACKAGE_PREFIX }/${ str }`;
 
 export const noop = (): void => {};
-
-export const removeKeyFromObject = (obj: Object, keyToRemove: string | number): Object => (
-  Object.keys(obj)
-    .filter(key => key === keyToRemove)
-    .reduce((newObj, key) => ({ ...newObj, [key]: obj[key] }), {})
-);
