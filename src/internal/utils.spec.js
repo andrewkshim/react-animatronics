3db87aabf6742f4f66ea159f3e1675c052a474d7
Andrew Shim <andrewkshim92@gmail.com>
2017-10-21 10:01:45 -0700

    :white_check_mark: [utils] Added test for makeError

---
+++ src/internal/utils.spec.js
@@ -0,0 +1,13 @@
+// @flow
+import test from 'tape'
+
+import { makeError } from './utils'
+
+test.only('utils.makeError', assert => {
+  const err = makeError('hello', 'world');
+  assert.equals(
+    err.message, 'hello\nworld',
+    'correctly formats the error message'
+  );
+  assert.end();
+});
