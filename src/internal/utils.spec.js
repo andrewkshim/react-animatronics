6b208cd02adfa574c30f7bc3e75dedd54217be44
Andrew Shim <andrewkshim92@gmail.com>
2017-10-21 14:55:18 -0700

    :white_check_mark: [utils] Update test for makeError

--- src/internal/utils.spec.js
+++ src/internal/utils.spec.js
@@ -4,10 +4,29 @@ import test from 'tape'
 import { makeError } from './utils'
 
 test('utils.makeError', assert => {
-  const err = makeError('hello', 'world');
+  const spaceErr = makeError('hello', 'world');
   assert.equals(
-    err.message, 'hello world',
-    'correctly formats the error message'
+    spaceErr.message, 'hello world',
+    'correctly formats spaces'
   );
+
+  const newlineErr = makeError('hello', '\n', 'world');
+  assert.equals(
+    newlineErr.message, 'hello\nworld',
+    'correctly formats newlines'
+  );
+
+  const spaceAndNewlineErr = makeError(
+    'there should be a space between',
+    'these lines but there should not',
+    '\n',
+    'be a space at the start of this line'
+  );
+  assert.equals(
+    spaceAndNewlineErr.message,
+    'there should be a space between these lines but there should not\nbe a space at the start of this line',
+    'correctly formats a combination of spaces and newlines'
+  );
+
   assert.end();
 });
