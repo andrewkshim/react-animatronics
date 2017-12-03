9230f855aed7e78fd3786092bc8f6d33b4924dec
Andrew Shim <andrewkshim92@gmail.com>
2017-12-02 17:15:22 -0800

    :sparkles: [utils] Added multiplyMatrices

--- src/internal/utils.js
+++ src/internal/utils.js
@@ -54,3 +54,47 @@ export const isUsingTime = (animation: Object): boolean =>
 export const isUsingSpring = (animation: Object): boolean =>
   animation.stiffness != null && animation.damping != null;
 
+// IMPROVE: If performance becomes an issue, look into using gl-matrix.
+export const multiplyMatrices = (matrixA, matrixB) => {
+  const [
+    ac0r0, ac1r0, ac2r0, ac3r0,
+    ac0r1, ac1r1, ac2r1, ac3r1,
+    ac0r2, ac1r2, ac2r2, ac3r2,
+    ac0r3, ac1r3, ac2r3, ac3r3,
+  ] = matrixA;
+
+  const [
+    bc0r0, bc1r0, bc2r0, bc3r0,
+    bc0r1, bc1r1, bc2r1, bc3r1,
+    bc0r2, bc1r2, bc2r2, bc3r2,
+    bc0r3, bc1r3, bc2r3, bc3r3,
+  ] = matrixB;
+
+  const c0r0 = (ac0r0 * bc0r0) + (ac1r0 * bc0r1) + (ac2r0 * bc0r2) + (ac3r0 * bc0r3);
+  const c1r0 = (ac0r0 * bc1r0) + (ac1r0 * bc1r1) + (ac2r0 * bc1r2) + (ac3r0 * bc1r3);
+  const c2r0 = (ac0r0 * bc2r0) + (ac1r0 * bc2r1) + (ac2r0 * bc2r2) + (ac3r0 * bc2r3);
+  const c3r0 = (ac0r0 * bc3r0) + (ac1r0 * bc3r1) + (ac2r0 * bc3r2) + (ac3r0 * bc3r3);
+
+  const c0r1 = (ac0r1 * bc0r0) + (ac1r1 * bc0r1) + (ac2r1 * bc0r2) + (ac3r1 * bc0r3);
+  const c1r1 = (ac0r1 * bc1r0) + (ac1r1 * bc1r1) + (ac2r1 * bc1r2) + (ac3r1 * bc1r3);
+  const c2r1 = (ac0r1 * bc2r0) + (ac1r1 * bc2r1) + (ac2r1 * bc2r2) + (ac3r1 * bc2r3);
+  const c3r1 = (ac0r1 * bc3r0) + (ac1r1 * bc3r1) + (ac2r1 * bc3r2) + (ac3r1 * bc3r3);
+
+  const c0r2 = (ac0r2 * bc0r0) + (ac1r2 * bc0r1) + (ac2r2 * bc0r2) + (ac3r2 * bc0r3);
+  const c1r2 = (ac0r2 * bc1r0) + (ac1r2 * bc1r1) + (ac2r2 * bc1r2) + (ac3r2 * bc1r3);
+  const c2r2 = (ac0r2 * bc2r0) + (ac1r2 * bc2r1) + (ac2r2 * bc2r2) + (ac3r2 * bc2r3);
+  const c3r2 = (ac0r2 * bc3r0) + (ac1r2 * bc3r1) + (ac2r2 * bc3r2) + (ac3r2 * bc3r3);
+
+  const c0r3 = (ac0r3 * bc0r0) + (ac1r3 * bc0r1) + (ac2r3 * bc0r2) + (ac3r3 * bc0r3);
+  const c1r3 = (ac0r3 * bc1r0) + (ac1r3 * bc1r1) + (ac2r3 * bc1r2) + (ac3r3 * bc1r3);
+  const c2r3 = (ac0r3 * bc2r0) + (ac1r3 * bc2r1) + (ac2r3 * bc2r2) + (ac3r3 * bc2r3);
+  const c3r3 = (ac0r3 * bc3r0) + (ac1r3 * bc3r1) + (ac2r3 * bc3r2) + (ac3r3 * bc3r3);
+
+  return [
+    c0r0, c1r0, c2r0, c3r0,
+    c0r1, c1r1, c2r1, c3r1,
+    c0r2, c1r2, c2r2, c3r2,
+    c0r3, c1r3, c2r3, c3r3,
+  ];
+};
+
